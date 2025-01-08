ebf33989b83c02a2a675cf89842dfc7793c0573f
starlash7 <yang92sw@gmail.com>
2025-01-09 00:20:14 +0900

    addLiquidity

--- Uniswap-v1/contracts/Exchange.sol
+++ Uniswap-v1/contracts/Exchange.sol
@@ -5,6 +5,14 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Exchange {
 
+    IERC20 token;
 
+    constructor(address _token) {
+        token = IERC20(_token);
+    }
+
+    function addLiquidity(uint256 _tokenAmount) public payable {
+        token.transferFrom(msg.sender, address(this), _tokenAmount);
+    }
 }
 
--- Uniswap-v1/test/Exchange.ts
+++ Uniswap-v1/test/Exchange.ts
@@ -1,7 +1,7 @@
-import { ethers } from "hardhat"
+import { ethers } from "hardhat";
 import { expect } from "chai";
 
-import { Exchange } from "../typechain-types/contracts/Exchange"
+import { Exchange } from "../typechain-types/contracts/Exchange";
 import { Token } from "../typechain-types/contracts/Token";
 import { BigNumber } from "ethers";
 
@@ -10,69 +10,30 @@ const toEther = (value: BigNumber) => ethers.utils.formatEther(value);
 const getBalance = ethers.provider.getBalance;
 
 describe("Exchange", () => {
-    let owner: any;
-    let user: any;
-    let exchange: Exchange;
-    let token: Token;
+  let owner: any;
+  let user: any;
+  let exchange: Exchange;
+  let token: Token;
 
-    beforeEach(async () => {
+  beforeEach(async () => {
+    //기본적으로 10,000개의 Ether를 가지고 있음.
+    [owner, user] = await ethers.getSigners();
+    const TokenFactory = await ethers.getContractFactory("Token");
+    token = await TokenFactory.deploy("GrayToken", "GRAY", toWei(1000000));
+    await token.deployed();
 
-        //기본적으로 10,000개의 Ether를 가지고 있음.
-        [owner, user] = await ethers.getSigners();
-        const TokenFactory = await ethers.getContractFactory("Token");
-        token = await TokenFactory.deploy("GrayToken", "GRAY", toWei(1000000));
-        await token.deployed();
+    const ExchangeFactory = await ethers.getContractFactory("Exchange");
+    exchange = await ExchangeFactory.deploy(token.address);
+    await exchange.deployed();
+  });
 
-        const ExchangeFactory = await ethers.getContractFactory("Exchange");
-        exchange = await ExchangeFactory.deploy(token.address);
-        await exchange.deployed();
+  describe("addLiquidity", async () => {
+    it("add Liquidty", async () => {
+      await token.approve(exchange.address, toWei(1000));
+      await exchange.addLiquidity(toWei(1000), { value: toWei(1000) });
+
+      expect(await getBalance(exchange.address)).to.equal(toWei(1000));
+      expect(await token.balanceOf(exchange.address)).to.equal(toWei(1000));
     });
-
-    describe("addLiquidity", async () => {
-        it("add liquidity", async () => {
-          await token.approve(exchange.address, toWei(1000));
-          await exchange.addLiquidity(toWei(1000), { value: toWei(1000) });
-          expect(await getBalance(exchange.address)).to.equal(toWei(1000));
-          expect(await token.balanceOf(exchange.address)).to.equal(toWei(1000));
-        });
-      });
-
-    describe("getTokenPrice", async() => {
-        it("correct get Token Price", async() => {
-            await token.approve(exchange.address, toWei(1000));
-            await exchange.addLiquidity(toWei(1000), { value: toWei(1000) });
-            
-            const tokenReserve = await token.balanceOf(exchange.address);
-            const etherReserve = await getBalance(exchange.address);
-
-            // GRAY Price
-            // Expect 1ETH per 1GRAY
-            expect(
-                (await exchange.getPrice(tokenReserve, etherReserve))
-            ).to.eq(1);
-        })
-    })
-
-    describe("EthToTokenSwap", async() => {
-        it("correct EthToTokenSwap", async() => {
-
-            await token.approve(exchange.address, toWei(1000));
-            await exchange.addLiquidity(toWei(1000), { value: toWei(1000) });
-      
-            await exchange.connect(user).ethToTokenSwap({ value: toWei(1) });
-
-            expect(
-                (toEther(await token.balanceOf(user.address)))
-            ).to.eq("1.0");
-
-            expect(
-                (toEther(await token.balanceOf(exchange.address)))
-            ).to.eq("999.0");
-
-            expect(
-                (toEther(await getBalance(exchange.address)))
-            ).to.eq("1001.0");
-            
-        })
-    })
-})
+  });
+});

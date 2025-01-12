31463ce237266213f88e71188abb327a6a200379
starlash7 <yang92sw@gmail.com>
2025-01-13 00:58:05 +0900

    Cleanup

--- ReactJS-Movie/react-for-beginners/src/App.js
+++ ReactJS-Movie/react-for-beginners/src/App.js
@@ -1,32 +1,24 @@
 import { useState, useEffect } from "react";
 
+function Hello() {
+  function byFn() {
+    console.log("bye :(");
+  }
+  function hiFn() {
+    console.log("created :)");
+    return byFn;
+  }
+  useEffect(hiFn, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log("i run only once.");
-  }, []);
-
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }

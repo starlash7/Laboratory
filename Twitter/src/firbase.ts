a213913a610e8c3fa9e8051437908bb675cb9112
starlash7 <yang92sw@gmail.com>
2025-01-21 23:39:24 +0900

    firebase set

---
+++ Twitter/src/firbase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC_NGkk578lpnsMbuhMZozo7e0bh9yrf8A",
+  authDomain: "twitter-reloaded-b757d.firebaseapp.com",
+  projectId: "twitter-reloaded-b757d",
+  storageBucket: "twitter-reloaded-b757d.firebasestorage.app",
+  messagingSenderId: "966955816705",
+  appId: "1:966955816705:web:326d697730913ca8d1ad00",
+};
+
+const app = initializeApp(firebaseConfig);

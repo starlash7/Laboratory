ca1f36e041d26992150110cd0c7d77180d2eebd1
starlash7 <yang92sw@gmail.com>
2025-01-24 23:58:35 +0900

    uploading images

--- Twitter/src/components/post-tweet-form.tsx
+++ Twitter/src/components/post-tweet-form.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import styled from "styled-components"
-import { auth, db } from "../firbase";
-import { addDoc, collection } from "firebase/firestore";
+import { auth, db, storage } from "../firbase";
+import { addDoc, collection, updateDoc } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 const Form = styled.form`
     display: flex;
@@ -86,12 +87,25 @@ export default function PostTweetForm() {
 
         try {
             setLoading(true);
-            await addDoc(collection(db, "tweets"), {
+            const doc = await addDoc(collection(db, "tweets"), {
                 tweet,
                 createdAt: Date.now(),
                 username: user.displayName || "Anonymous",
                 userId: user.uid,
             })
+            if (file) {
+                const locationRef = ref(
+                    storage,
+                    `tweets/${user.uid}-${user.displayName}/${doc.id}`
+                );
+                const result = await uploadBytes(locationRef, file);
+                const url = getDownloadURL(result.ref);
+                await updateDoc(doc, {
+                    photo: url
+                })
+            }
+            setTweet("");
+            setFile(null);
         } catch (e) {
             console.log(e);
         } finally {
@@ -101,6 +115,7 @@ export default function PostTweetForm() {
 
     return <Form onSubmit={onSubmit}>
         <TextArea
+            required
             rows={5}
             maxLength={180}
             onChange={onChange} value={tweet} placeholder="What is happening?" />

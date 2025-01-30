0d3dd0c5d756be83fe0ec362bfbcf12162bd1525
starlash7 <yang92sw@gmail.com>
2025-01-30 22:06:08 +0900

    useQuery Variables

--- graphql-client/src/routes/Movie.js
+++ graphql-client/src/routes/Movie.js
@@ -1,3 +1,24 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+const GET_MOVIE = gql`
+  query getMovie($movieId: String!) {
+    movie(id: $movieId) {
+      id
+      title
+    }
+  }
+`;
+
 export default function Movie() {
-  return <div>This is movie detail</div>;
+  const { id } = useParams();
+  const { data, loading } = useQuery(GET_MOVIE, {
+    variables: {
+      movieId: id,
+    },
+  });
+  console.log(data, loading);
+  if (loading) {
+    return <h1>Fetching movie...</h1>;
+  }
+  return <div>{data.movie.title}</div>;
 }
--- graphql-client/src/routes/Movies.js
+++ graphql-client/src/routes/Movies.js
@@ -1,23 +1,17 @@
 import { gql, useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
 
-const GET_MOVIES = gql`
+const ALL_MOVIES = gql`
   query getMovies {
     allMovies {
       title
       id
     }
-    allTweets {
-      id
-      text
-      author {
-        fullName
-      }
-    }
   }
 `;
 
 export default function Movies() {
-  const { data, loading, error } = useQuery(GET_MOVIES);
+  const { data, loading, error } = useQuery(ALL_MOVIES);
   if (loading) {
     return <h1>Loading...</h1>;
   }
@@ -28,12 +22,8 @@ export default function Movies() {
     <ul>
       <h1>Movies</h1>
       {data.allMovies.map((movie) => (
-        <li key={movie.id}>{movie.title}</li>
-      ))}
-      <h1>Tweets</h1>
-      {data.allTweets.map((tweet) => (
-        <li key={tweet.id}>
-          {tweet.text}/by: {tweet.author.fullName}
+        <li key={movie.id}>
+          <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
         </li>
       ))}
     </ul>
